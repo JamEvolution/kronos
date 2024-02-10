c600f5431340fcee4432b37e58a60ce7bba3167c
Murat Zabun <53602305+JamEvolution@users.noreply.github.com>
2024-02-11 00:20:06 +0300

    Application initialize file created

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:kronos/feature/home/view/home_view.dart';
+import 'package:kronos/product/init/application_initialize.dart';
 import 'package:kronos/product/init/product_localization.dart';
 import 'package:kronos/product/utility/constants/app/app_constants.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-
-  runApp(ProductLocalization(child: const MyApp()));
+Future <void> main() async {
+  await ApplicationInitialize().make();
+  runApp(ProductLocalization(child: const _MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyApp extends StatelessWidget {
+  const _MyApp();
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-       home: const HomeView(),
+      home: const HomeView(),
     );
   }
 }
