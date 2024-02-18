e07e40cd1e3c41980db851bbc0ea1aecb2373b45
Murat Zabun <53602305+JamEvolution@users.noreply.github.com>
2024-02-19 02:31:48 +0300

    preview devices package done and use the launch.json!

---
+++ lib/development/preview_main.dart
@@ -0,0 +1,39 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:kronos/product/init/application_initialize.dart';
+import 'package:kronos/product/init/product_localization.dart';
+import 'package:kronos/product/init/theme/custom_dark_theme.dart';
+import 'package:kronos/product/init/theme/custom_light_theme.dart';
+import 'package:kronos/product/navigation/app_router.dart';
+import 'package:kronos/product/utility/constants/app/app_constants.dart';
+import 'package:uiwidget/uiwidget.dart';
+import 'package:device_preview/device_preview.dart';
+
+Future<void> main() async {
+  await ApplicationInitialize().make();
+  runApp(
+    DevicePreview(
+      builder: (context) =>
+           ProductLocalization(child: const _MyApp()),
+    ),
+  );
+}
+
+final class _MyApp extends StatelessWidget {
+  const _MyApp();
+  static final _appRouter = AppRouter();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      builder: CustomResponsive.build,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      debugShowCheckedModeBanner: false,
+      title: ApplicationConstants.COMPANY_NAME,
+      theme: CustomLightTheme().themeData,
+      darkTheme: CustomDarkTheme().themeData,
+      routerConfig: _appRouter.config(),
+    );
+  }
+}
